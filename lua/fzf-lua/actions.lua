12f2a8eb2138d30cb67bac5d3fde0e9b91a18b3e
Ken Maeshima <81536479+mononoken@users.noreply.github.com>
2025-01-09 17:52:14 -0800

    Utilize save_dialog util for close confirmation

--- lua/fzf-lua/actions.lua
+++ lua/fzf-lua/actions.lua
@@ -405,9 +405,12 @@ M.buf_switch_or_edit = M.file_switch_or_edit
 M.buf_del = function(selected, opts)
   for _, sel in ipairs(selected) do
     local entry = path.entry_to_file(sel, opts)
-    local force = not utils.buffer_is_dirty(entry.bufnr, true, false)
     if entry.bufnr then
-      pcall(vim.api.nvim_buf_delete, entry.bufnr, { force = force })
+      if not utils.buffer_is_dirty(entry.bufnr, true, false) then
+        vim.api.nvim_buf_delete(entry.bufnr, { force = true })
+      elseif utils.save_dialog(entry.bufnr) then
+        vim.api.nvim_buf_delete(entry.bufnr, { force = true })
+      end
     end
   end
 end
